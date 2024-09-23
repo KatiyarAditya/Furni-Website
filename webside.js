1c19b074c227c32192044ad31436242790502af7
AdityaKatiyar <katiyaraditya7236@gmail.com>
2024-09-23 17:29:44 +0530

    Add files via upload

---
+++ webside.js
@@ -0,0 +1,11 @@
+function nextButton1(){
+    document.getElementById("slide1") .style.display="none";
+    document.getElementById("slide2") .style.display="block";
+   
+}
+
+function preButton1(){
+     document.getElementById("slide1") .style.display="block";
+    document.getElementById("slide2") .style.display="none";
+}
+
